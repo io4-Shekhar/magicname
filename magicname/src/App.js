9196e8fec68a280eb237aa58a19dc146f2621b2e
Shekhar <io4.shekhar@gmail.com>
2023-07-16 17:18:51 +0530

    trial

---
+++ magicname/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import button from "./pages/button";
+
+function App() {
+  return (
+    <div className="App">
+      <button />
+    </div>
+  );
+}
+
+export default App;
---
+++ magicname/src/pages/button.js
@@ -0,0 +1,11 @@
+import button from "react";
+
+const button = () => {
+  return (
+    <div>
+      <button variant="text">button1</button>
+      <button variant="text">button2</button>
+    </div>
+  );
+};
+export default button;
